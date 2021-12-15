6a4bb29274a0d507bda8cc22f849ba1384e378b1
mohamed  zakaria <m.zakariaa06@gmail.com>
2021-12-15 06:42:08 +0200

    aa

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 //global var
 const strat = document.querySelector('.strat-zekr');
 const stratbtn = document.querySelector('#startBtn');
-
-const badge = document.querySelector('.badge')
-// const badge2 = array.form(document.querySelector('.badge'))
-const azkarBlock = Array.from(document.querySelectorAll('.azkar-block'));
+const btnCount = document.getElementsByClassName('btn-count');
+const spanNum = document.getElementsByClassName('btn-number');
 const conter = document.querySelector('.counter');
 const plus = document.getElementById('plus');
 const redo = document.getElementById('redo');
@@ -15,29 +13,59 @@ const massaa = document.querySelector('.massaa');
 strat.addEventListener('click', function () {
     this.remove();
 })
-//loop
-azkarBlock.forEach(function (zakr) {
+//click the btn to count 
+let countBtn = () => {
+    for (let i = 0; i < btnCount.length; i++) {
+        let btnCountt = btnCount[i];
+        btnCountt.addEventListener('click', function (e) {
+            let btnClicked = e.target;
+            console.log(btnClicked)
+            // if (parseInt(btnClicked.lastElementChild.textContent) == parseInt(btnClicked.lastChild.value)) {
+            //     alert(1)
+            //     let val = parseInt(btnClicked.lastElementChild.textContent);
+            //     val++;
+            //     btnClicked.lastElementChild.textContent = val;
 
-    zakr.addEventListener('click', function () {
 
-        handelBadge();
-    })
+            // }
 
-});
-function handelBadge() {
-    // let theInnnerText = parseInt(badge.textContent) - 1;
-    // badge.textContent = theInnnerText;
-    // if (theInnnerText === 0) {
+        })
 
-    //     badge.classList.remove('bg-danger')
-    //     badge.classList.add('bg-warning');
-    //     badge.classList.add('text-dark');
-    //     badge.classList.add('text-bold');
-    //     e.classList.add('noClicking');
-    console.log(1)
-    // e.remove()
+    }
 }
+// azkarBlock.map(function (zakr, i) {
+//     badge.map(function (bad, j) {
+//         zakr.addEventListener('click', function () {
 
+//             handelBadge(bad[0], zakr[0]);
+
+
+
+//         })
+
+
+//     })
+
+
+
+// });
+
+// function handelBadge(badge, container) {
+//     let theInnnerText = parseInt(badge.textContent) - 1;
+//     badge.textContent = theInnnerText;
+//     if (badge.dataset.count === container.dataset.count && theInnnerText === 0) {
+
+//         badge.classList.remove('bg-danger')
+//         badge.classList.add('bg-warning');
+//         badge.classList.add('text-dark');
+//         badge.classList.add('text-bold');
+//         container.classList.add('noClicking');
+//         container.lastChild
+
+//         // container.remove()
+//     }
+//     console.log(1)
+// }
 // }
 //counter handler
 plus.addEventListener('click', function () {
@@ -57,13 +85,15 @@ sebha.addEventListener('click', function () {
     conter.classList.toggle('counter-show');
     this.classList.toggle('sebha-margin')
 })
-//massa 
-// massaa.addEventListener('click', function () {
-//     document.body.style.backgroundImage = '../img/back.jpg';
-// })
+
 function AzkarMasaa() {
     document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80')"
 }
 function AzkarSabah() {
     document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1566228015668-4c45dbc4e2f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80')"
 }
+//************************************************************** */
+// const badge = document.querySelector('.badge')
+// const badge = Array.from(document.querySelectorAll('.badge'))
+// const azkarBlock = Array.from(document.querySelectorAll('.azkar-block'));
+// const azkarBlocks = document.getElementsByClassName('card');
