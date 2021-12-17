d91a4f6429f7e847e76740b586eef56c5856d1af
mohamed  zakaria <m.zakariaa06@gmail.com>
2021-12-17 06:57:45 +0200

    responsive background

--- js/main.js
+++ js/main.js
@@ -87,7 +87,7 @@ sebha.addEventListener('click', function () {
 })
 
 function AzkarMasaa() {
-    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80')"
+    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1548625361-2341a17236b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80')"
 }
 function AzkarSabah() {
     document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1542664408056-8ec64f24de47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NjJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60')"
